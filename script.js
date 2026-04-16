5c1c61142e83490994ae0d3fe7f1f9639026b74d
melaya-j <melaya710@gmail.com>
2026-04-15 23:05:34 -0500

    event listener and slides. partial done slides.

--- script.js
+++ script.js
@@ -20,4 +20,26 @@ function init(){
 	});
 
 	var contentString = '<h1>IIT Tower</h1><p>IIT Tower is on the south end of the Mies IIT campus. The official name is Micheal Paul Galvin Tower. IIT Tower is on the south end of the Mies IIT campus. The official name is Micheal Paul Galvin Tower. IIT Tower is on the south end of the Mies IIT campus. The official name is Micheal Paul Galvin Tower. IIT Tower is on the south end of the Mies IIT campus. The official name is Micheal Paul Galvin Tower.IIT Tower is on the south end of the Mies IIT campus. The official name is Micheal Paul Galvin Tower. IIT Tower is on the south end of the Mies IIT campus. The official name is Micheal Paul Galvin Tower. IIT Tower is on the south end of the Mies IIT campus. The official name is Micheal Paul Galvin Tower. IIT Tower is on the south end of the Mies IIT campus. The official name is Micheal Paul Galvin Tower.</p>';
+
+	var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  	});
+
+	google.maps.event.addListener(marker, 'mouseover', function() {
+    	infowindow.open(myMap, marker);
+  	});
+
+
+
+}
+
+google.maps.event.addDomListener(window, 'load', init);
+
+
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+  const slideWidth = slides[0].clientWidth;
+  document.querySelector('.slides').style.transform = `translateX(-${index * slideWidth}px)`;
 }
